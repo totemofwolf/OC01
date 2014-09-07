765fe55e1fae0e094826d9f465f24acc23894f24
wand <wand@WestC0de.local>
2014-09-07 21:33:02 +0800

    Initial Commit

---
+++ OC01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC01
+//
+//  Created by wand on 14-9-7.
+//  Copyright (c) 2014年 wand. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
